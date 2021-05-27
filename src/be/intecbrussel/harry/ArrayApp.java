8b2c59cd213878fb443649dc448fe56ac9395aec
Lvschoor <lvschoor@gmail.com>
2021-05-27 16:04:08 +0200

    Added ArrayApp and IntArrayTools

--- src/be/intecbrussel/harry/ArrayApp.java
+++ src/be/intecbrussel/harry/ArrayApp.java
@@ -1,10 +1,15 @@
 package be.intecbrussel.harry;
 
 
+import java.util.Arrays;
+
 public class ArrayApp {
 
     public static void main(String[] args) {
-	int[] testArray=  {9,5,3};
-        System.out.println(IntArrayTools.isSorted(testArray, false));
+	int[] testArray=  {1,5,3,6};
+        System.out.println(IntArrayTools.isSorted(testArray, true));
+        System.out.println(Arrays.toString(IntArrayTools.sort(testArray,true)));
+        System.out.println(Arrays.toString(IntArrayTools.sort(testArray,false)));
+
     }
 }
--- src/be/intecbrussel/harry/IntArrayTools.java
+++ src/be/intecbrussel/harry/IntArrayTools.java
@@ -1,37 +1,47 @@
 package be.intecbrussel.harry;
 
 public class IntArrayTools {
-
+    // testing if the array is sorted up or down depending on value of sortUp
     public static boolean isSorted(int[] arrayOfInts, boolean sortUp) {
         boolean sorted = false;
 
-        if (sortUp) {
-            for (int i = 0; i < arrayOfInts.length; i++) {
-                for (int j = i + 1; j < arrayOfInts.length; j++) {
-                    if (arrayOfInts[i] <= arrayOfInts[j]) {
-                        sorted = true;
-                    } else {
-                        sorted = false;
-                        break;
-                    }
+        for (int i = 0; i < arrayOfInts.length; i++) {
+            for (int j = i + 1; j < arrayOfInts.length; j++) {
+                if (((arrayOfInts[i] <= arrayOfInts[j]) && sortUp) || ((arrayOfInts[i] >= arrayOfInts[j]) && !sortUp)) {
+                    sorted = true;
+                } else {
+                    sorted = false;
+                    break;
                 }
-                if (sorted==false) break;
             }
-            return sorted;
-        } else {
-            for (int i = 0; i < arrayOfInts.length; i++) {
-                for (int j = i + 1; j < arrayOfInts.length; j++) {
-                    if (arrayOfInts[i] >= arrayOfInts[j]) {
-                        sorted = true;
-                    } else {
-                        sorted = false;
-                        break;
-                    }
-                }
-                if (sorted==false) break;
-            }
-            return sorted;
+            if (!sorted) break;
         }
+        return sorted;
+    }
 
+    public static int[] sort(int[] arrayOfInts, boolean sortUp) {
+
+        int temp;
+        for (int i = 0; i < arrayOfInts.length; i++) {
+            for (int j = i; j < arrayOfInts.length - 1; j++) {
+                int index = i;
+
+                if (sortUp) {
+
+                    if (arrayOfInts[j + 1] < arrayOfInts[index]) {
+                        index = j + 1;
+                    }
+                } else {
+                    if (arrayOfInts[j + 1] > arrayOfInts[index]) {
+                        index = j + 1;
+                    }
+                }
+                temp = arrayOfInts[i];
+                arrayOfInts[i] = arrayOfInts[index];
+                arrayOfInts[index] = temp;
+            }
+        }
+        return arrayOfInts;
     }
 }
+
